d1950e304b971e78a9314334171a4de0b813f293
Khushal Mali <khushalmali8448@gmail.com>
2024-03-30 23:33:13 +0530

    created middleware

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,24 +1,50 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
+
+type Variables = {
+  userId: string;
+};
 
 export const blogRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
   };
+  Variables: Variables;
 }>();
 
-blogRouter.post("/api/v1/blog", (c) => {
+blogRouter.use(async (c, next) => {
+  // console.log(c);
+  const reqHeader = c.req.header("Authorization");
+  if (!reqHeader || !reqHeader.startsWith("Bearer ")) {
+    return c.text("Unauthorized");
+  }
+
+  const token = reqHeader.split(" ")[1];
+  const decodedToken = decode(token);
+
+  c.set("userId", decodedToken.payload.id);
+
+  await next();
+});
+
+blogRouter.post("/", (c) => {
+  const userId = c.get("userId");
+  console.log(userId);
+
   return c.text("Hello Hono!");
 });
 
-blogRouter.put("/api/v1/blog", (c) => {
+blogRouter.put("/", (c) => {
   return c.text("Hello Hono hihihi");
 });
 
-blogRouter.get("/api/v1/blog/bulk", (c) => {
+blogRouter.get("/bulk", (c) => {
   return c.text("Hello all blog!");
 });
 
-blogRouter.get("/api/v1/blog/:id", (c) => {
+blogRouter.get("/:id", (c) => {
   return c.text("Hello Hono! hihihi");
 });
