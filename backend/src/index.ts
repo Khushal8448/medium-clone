c78c93f3823d8e0e348be4d4831486cb86dc6b58
Khushal Mali <khushalmali8448@gmail.com>
2024-03-30 20:23:34 +0530

    created signup and signin routes and functionalities in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,25 @@
 import { Hono } from "hono";
+import z from "zod";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
+
+const zodSignup = z.object({
+  username: z.string().email(),
+  password: z.string().min(6),
+  firstName: z.string().min(1),
+  lastName: z.string().optional(),
+});
+
+const zodSignin = z.object({
+  username: z.string().email(),
+  password: z.string().min(6),
+});
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
@@ -12,16 +27,69 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.post("/api/v1/user/signup", (c) => {
+app.post("/api/v1/user/signup", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
 
-  return c.text("Hello SignUp");
+  const body = await c.req.json();
+  const { success } = zodSignup.safeParse(body);
+
+  if (success) {
+    try {
+      const user = await prisma.user.create({
+        data: {
+          username: body.username,
+          password: body.password,
+          firstName: body.firstName,
+          lastName: body.lastName,
+        },
+      });
+
+      const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+      console.log(jwt);
+
+      return c.json({ jwt });
+    } catch (error: any) {
+      c.status(403);
+      return c.text(error.message);
+    }
+  }
+
+  return c.text("Invalid Inputs!!");
 });
 
-app.post("/api/v1/user/signin", (c) => {
-  return c.text("Hello SignIn");
+app.post("/api/v1/user/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body: z.infer<typeof zodSignin> = await c.req.json();
+
+  const { success } = zodSignin.safeParse(body);
+
+  if (success) {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          username: body.username,
+          password: body.password,
+        },
+      });
+
+      if (user === null) return c.text("User does not exies!");
+
+      const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+      console.log(jwt);
+
+      return c.json({ jwt });
+    } catch (error: any) {
+      c.status(403);
+      return c.text(error.message);
+    }
+  }
+
+  return c.text("Invalid Inputs!!");
 });
 
 app.post("/api/v1/blog", (c) => {
